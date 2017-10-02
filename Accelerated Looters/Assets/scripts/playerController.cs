86348828bd289612b721c69d2e2718af89f92cb1
Yiyuan <monicayung@users.noreply.github.com>
2017-10-01 20:23:00 -0600

    revised version of ENEMIGO_1.unity scene

---
+++ Accelerated Looters/Assets/scripts/playerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+	public float moveSpeed;
+	public Rigidbody2D myRigidbody;
+	
+	
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw("Horizontal")>0f)
+		{
+			myRigidbody.velocity=new Vector3(moveSpeed, myRigidbody.velocity.y,0);
+			
+		}
+		if (Input.GetAxisRaw("Vertical")>0f)
+		{
+			myRigidbody.velocity=new Vector3(myRigidbody.velocity.x,moveSpeed,0);
+			
+		}
+	}
+}
